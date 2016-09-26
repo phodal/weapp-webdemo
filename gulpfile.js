c7669e240855591e14b7ec12bb7e22aac8b5cfac
Fengda HUANG <h@phodal.com>
2016-09-26 17:42:32 +0800

    add simple build scripts

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    exec = require('child_process').exec;
+
+gulp.task('watch', function() {
+    var watcher = gulp.watch('src/**/*.wxml');
+    watcher.on('change', function(event) {
+        var outputFileName = event.path.substring(0, event.path.length - 4) + 'js';
+        outputFileName = outputFileName.replace('/src/', '/build/');
+        console.log(outputFileName);
+        exec('./vendor/wcc -d ' + event.path + ' > ' + outputFileName, function(err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+        });
+    });
+});
